57b8c6cd7c492ce72c059bff8f1f14d626a14209
David <dhergon19@gmail.com>
2023-10-24 10:46:44 -0600

    Final

--- docs/JS/Index.js
+++ docs/JS/Index.js
@@ -1,12 +1,15 @@
 const observer = new IntersectionObserver((entries) => {
-    entries.forEach(entry => {
-        console.log(entry);
+    entries.forEach(entry => {  
         if (entry.isIntersecting) {
-            entry.target.classList.add('show')
+            entry.target.classList.add('showw')
         }else{
-            entry.target.classList.remove('show')
+            entry.target.classList.remove('showw')
         }
     });
 })
 const hiddenElements = document.querySelectorAll(".hidden")
 hiddenElements.forEach((el) => observer.observe(el))
+if (screen.width<=800){
+    $("td.r").replaceWith("<tr>",$("td.r"),"</tr>")
+}
+
